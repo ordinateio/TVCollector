3108bc59f835dd14221a71324cdc017493563290
Callisto <callisto2410@yandex.ru>
2019-02-13 21:47:07 +0900

    returned OnLoadWebDocument

--- _build/data/transport.plugins.php
+++ _build/data/transport.plugins.php
@@ -1,13 +1,13 @@
 <?php
 function getPluginContent($filename) {
   $output = file_get_contents($filename);
-  $output = trim( str_replace(array('<?php', '?>'), '', $output) );
+  $output = trim(str_replace(array('<?php', '?>'), '', $output));
   return $output;
 }
 
 
 
-// =============================================================================
+// -----------------------------------------------------------------------------
 // plugins
 $plugins = array();
 
@@ -18,11 +18,11 @@ $plugins[1]->fromArray(array(
   'description' => '',
   'plugincode'  => getPluginContent($sources['elements'] . 'plugins/plugin.tvcollector.php'),
 ), '', true, true);
-// =============================================================================
+// -----------------------------------------------------------------------------
 
 
 
-// =============================================================================
+// -----------------------------------------------------------------------------
 // events
 $events = array();
 
@@ -33,10 +33,17 @@ $events['OnDocFormSave']->fromArray(array(
   'propertyset' => 0
 ), '', true, true);
 
+$events['OnLoadWebDocument'] = $modx->newObject('modPluginEvent');
+$events['OnLoadWebDocument']->fromArray(array(
+  'event'       => 'OnLoadWebDocument',
+  'priority'    => 0,
+  'propertyset' => 0
+), '', true, true);
+
 $plugins[1]->addMany($events);
 $modx->log(xPDO::LOG_LEVEL_INFO, 'Packaged in ' . count($events) . ' Plugin Events.'); flush();
 unset($events);
-// =============================================================================
+// -----------------------------------------------------------------------------
 
 
 
