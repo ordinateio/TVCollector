eb584bda4af64d9921887439bc167c0e9905cb3c
Callisto <callisto2410@yandex.ru>
2019-04-08 21:13:25 +0900

    minor changes

--- _build/build.config.sample.php
+++ _build/build.config.sample.php
@@ -1,5 +1,5 @@
 <?php
-// define the MODX path constants necessary for core installation
+// Define the MODX path constants necessary for core installation
 define('MODX_BASE_PATH', dirname(dirname(dirname(__FILE__))) . '/');
 define('MODX_BASE_URL',  '/');
 define('MODX_CORE_PATH', MODX_BASE_PATH . 'core/');
